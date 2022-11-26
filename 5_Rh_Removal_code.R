6d324ae8287a60a1b7bb732984182fdd2032c1d4
kygithubtokenaccount <94480321+kygithubtokenaccount@users.noreply.github.com>
2022-11-25 18:34:18 -0600

    Update for final publication

--- 5_Rh_Removal_code.R
+++ 5_Rh_Removal_code.R
@@ -20,16 +20,22 @@ a1dr.data <- as.matrix(GetAssayData(a1dr, slot = "counts", assay = "RNA"))
 a1drh34.data <- a1dr.data[-(which(rownames(a1dr.data) %in% c("Rh3","Rh4"))),]
 a1drh56.data <- a1dr.data[-(which(rownames(a1dr.data) %in% c("Rh5","Rh6"))),]
 a1dra.data <- a1dr.data[-(which(rownames(a1dr.data) %in% c("Rh3","Rh4","Rh5","Rh6"))),]
+a1drninae.data <- a1dr.data[-(which(rownames(a1dr.data) %in% c("ninaE"))),]
+a1drall.data <- a1dr.data[-(which(rownames(a1dr.data) %in% c("ninaE","Rh3","Rh4","Rh5","Rh6"))),]
 
 a3dr.data <- as.matrix(GetAssayData(a3dr, slot = "counts", assay = "RNA"))
 a3drh34.data <- a3dr.data[-(which(rownames(a3dr.data) %in% c("Rh3","Rh4"))),]
 a3drh56.data <- a3dr.data[-(which(rownames(a3dr.data) %in% c("Rh5","Rh6"))),]
 a3dra.data <- a3dr.data[-(which(rownames(a3dr.data) %in% c("Rh3","Rh4","Rh5","Rh6"))),]
+a3drninae.data <- a3dr.data[-(which(rownames(a1dr.data) %in% c("ninaE"))),]
+a3drall.data <- a3dr.data[-(which(rownames(a1dr.data) %in% c("ninaE","Rh3","Rh4","Rh5","Rh6"))),]
 
 a7dr.data <- as.matrix(GetAssayData(a7dr, slot = "counts", assay = "RNA"))
 a7drh34.data <- a7dr.data[-(which(rownames(a7dr.data) %in% c("Rh3","Rh4"))),]
 a7drh56.data <- a7dr.data[-(which(rownames(a7dr.data) %in% c("Rh5","Rh6"))),]
 a7dra.data <- a7dr.data[-(which(rownames(a7dr.data) %in% c("Rh3","Rh4","Rh5","Rh6"))),]
+a7drninae.data <- a7dr.data[-(which(rownames(a1dr.data) %in% c("ninaE"))),]
+a7drall.data <- a7dr.data[-(which(rownames(a1dr.data) %in% c("ninaE","Rh3","Rh4","Rh5","Rh6"))),]
 
 # We run Seurat scTransform workflow on these count matrices
 
@@ -37,40 +43,58 @@ a7dra.data <- a7dr.data[-(which(rownames(a7dr.data) %in% c("Rh3","Rh4","Rh5","Rh
 a1dr34 <- CreateSeuratObject(counts = a1drh34.data, project = "a1d_remove_Rh34")
 a1dr56 <- CreateSeuratObject(counts = a1drh56.data, project = "a1d_remove_Rh56")
 a1dra <- CreateSeuratObject(counts = a1dra.data, project = "a1d_remove_Rh")
+a1drninae <- CreateSeuratObject(counts = a1drninae.data, project = "a1d_remove_ninaE")
+a1drall <- CreateSeuratObject(counts = a1drall.data, project = "a1d_remove_all_Rh")
 
 a3dr34 <- CreateSeuratObject(counts = a3drh34.data, project = "a3d_remove_Rh34")
 a3dr56 <- CreateSeuratObject(counts = a3drh56.data, project = "a3d_remove_Rh56")
 a3dra <- CreateSeuratObject(counts = a3dra.data, project = "a3d_remove_Rh")
+a3drninae <- CreateSeuratObject(counts = a3drninae.data, project = "a3d_remove_ninaE")
+a3drall <- CreateSeuratObject(counts = a3drall.data, project = "a3d_remove_all_Rh")
 
 a7dr34 <- CreateSeuratObject(counts = a7drh34.data, project = "a7d_remove_Rh34")
 a7dr56 <- CreateSeuratObject(counts = a7drh56.data, project = "a7d_remove_Rh56")
 a7dra <- CreateSeuratObject(counts = a7dra.data, project = "a7d_remove_Rh")
+a7drninae <- CreateSeuratObject(counts = a7drninae.data, project = "a7d_remove_ninaE")
+a7drall <- CreateSeuratObject(counts = a7drall.data, project = "a7d_remove_all_Rh")
 
 # Add in percent mitochondrial genes metadata
 a1dr34 <- PercentageFeatureSet(a1dr34, pattern = "mt:", col.name = "percent.mt")
 a1dr56 <- PercentageFeatureSet(a1dr56, pattern = "mt:", col.name = "percent.mt")
 a1dra <- PercentageFeatureSet(a1dra, pattern = "mt:", col.name = "percent.mt")
+a1drninae <- PercentageFeatureSet(a1drninae, pattern = "mt:", col.name = "percent.mt")
+a1drall <- PercentageFeatureSet(a1drall, pattern = "mt:", col.name = "percent.mt")
 
 a3dr34 <- PercentageFeatureSet(a3dr34, pattern = "mt:", col.name = "percent.mt")
 a3dr56 <- PercentageFeatureSet(a3dr56, pattern = "mt:", col.name = "percent.mt")
 a3dra <- PercentageFeatureSet(a3dra, pattern = "mt:", col.name = "percent.mt")
+a3drninae <- PercentageFeatureSet(a3drninae, pattern = "mt:", col.name = "percent.mt")
+a3drall <- PercentageFeatureSet(a3drall, pattern = "mt:", col.name = "percent.mt")
 
 a7dr34 <- PercentageFeatureSet(a7dr34, pattern = "mt:", col.name = "percent.mt")
 a7dr56 <- PercentageFeatureSet(a7dr56, pattern = "mt:", col.name = "percent.mt")
 a7dra <- PercentageFeatureSet(a7dra, pattern = "mt:", col.name = "percent.mt")
+a7drninae <- PercentageFeatureSet(a7drninae, pattern = "mt:", col.name = "percent.mt")
+a7drall <- PercentageFeatureSet(a7drall, pattern = "mt:", col.name = "percent.mt")
 
 # SCTransform
 a1dr34 <- SCTransform(a1dr34, variable.features.n = 3500, vars.to.regress = "percent.mt", verbose = TRUE)
 a1dr56 <- SCTransform(a1dr56, variable.features.n = 3500, vars.to.regress = "percent.mt", verbose = TRUE)
 a1dra <- SCTransform(a1dra, variable.features.n = 3500, vars.to.regress = "percent.mt", verbose = TRUE)
+a1drninae <- SCTransform(a1drninae, variable.features.n = 3500, vars.to.regress = "percent.mt", verbose = TRUE)
+a1drall <- SCTransform(a1drall, variable.features.n = 3500, vars.to.regress = "percent.mt", verbose = TRUE)
 
 a3dr34 <- SCTransform(a3dr34, variable.features.n = 2800, vars.to.regress = "percent.mt", verbose = TRUE)
 a3dr56 <- SCTransform(a3dr56, variable.features.n = 2800, vars.to.regress = "percent.mt", verbose = TRUE)
 a3dra <- SCTransform(a3dra, variable.features.n = 2800, vars.to.regress = "percent.mt", verbose = TRUE)
+a3drninae <- SCTransform(a3drninae, variable.features.n = 2800, vars.to.regress = "percent.mt", verbose = TRUE)
+a3drall <- SCTransform(a3drall, variable.features.n = 2800, vars.to.regress = "percent.mt", verbose = TRUE)
 
 a7dr34 <- SCTransform(a7dr34, variable.features.n = 2700, vars.to.regress = "percent.mt", verbose = TRUE)
 a7dr56 <- SCTransform(a7dr56, variable.features.n = 2700, vars.to.regress = "percent.mt", verbose = TRUE)
 a7dra <- SCTransform(a7dra, variable.features.n = 2700, vars.to.regress = "percent.mt", verbose = TRUE)
+a7drninae <- SCTransform(a7drninae, variable.features.n = 2700, vars.to.regress = "percent.mt", verbose = TRUE)
+a7drall <- SCTransform(a7drall, variable.features.n = 2700, vars.to.regress = "percent.mt", verbose = TRUE)
 
 # clustering with 50 dimensions
 a1dr34 <- RunPCA(a1dr34, verbose = TRUE)
@@ -88,6 +112,16 @@ a1dra <- RunUMAP(a1dra, dims = 1:50, verbose = TRUE)
 a1dra <- FindNeighbors(a1dra, dims = 1:50, verbose = TRUE)
 a1dra <- FindClusters(a1dra, verbose = TRUE)
 
+a1drninae <- RunPCA(a1drninae, verbose = TRUE)
+a1drninae <- RunUMAP(a1drninae, dims = 1:50, verbose = TRUE)
+a1drninae <- FindNeighbors(a1drninae, dims = 1:50, verbose = TRUE)
+a1drninae <- FindClusters(a1drninae, verbose = TRUE)
+
+a1drall <- RunPCA(a1drall, verbose = TRUE)
+a1drall <- RunUMAP(a1drall, dims = 1:50, verbose = TRUE)
+a1drall <- FindNeighbors(a1drall, dims = 1:50, verbose = TRUE)
+a1drall <- FindClusters(a1drall, verbose = TRUE)
+
 #
 a3dr34 <- RunPCA(a3dr34, verbose = TRUE)
 a3dr34 <- RunUMAP(a3dr34, dims = 1:50, verbose = TRUE)
@@ -103,6 +137,16 @@ a3dra <- RunPCA(a3dra, verbose = TRUE)
 a3dra <- RunUMAP(a3dra, dims = 1:50, verbose = TRUE)
 a3dra <- FindNeighbors(a3dra, dims = 1:50, verbose = TRUE)
 a3dra <- FindClusters(a3dra, verbose = TRUE)
+
+a3drninae <- RunPCA(a3drninae, verbose = TRUE)
+a3drninae <- RunUMAP(a3drninae, dims = 1:50, verbose = TRUE)
+a3drninae <- FindNeighbors(a3drninae, dims = 1:50, verbose = TRUE)
+a3drninae <- FindClusters(a3drninae, verbose = TRUE)
+
+a3drall <- RunPCA(a3drall, verbose = TRUE)
+a3drall <- RunUMAP(a3drall, dims = 1:50, verbose = TRUE)
+a3drall <- FindNeighbors(a3drall, dims = 1:50, verbose = TRUE)
+a3drall <- FindClusters(a3drall, verbose = TRUE)
 
 #
 a7dr34 <- RunPCA(a7dr34, verbose = TRUE)
@@ -120,6 +164,16 @@ a7dra <- RunUMAP(a7dra, dims = 1:50, verbose = TRUE)
 a7dra <- FindNeighbors(a7dra, dims = 1:50, verbose = TRUE)
 a7dra <- FindClusters(a7dra, verbose = TRUE)
 
+a7drninae <- RunPCA(a7drninae, verbose = TRUE)
+a7drninae <- RunUMAP(a7drninae, dims = 1:50, verbose = TRUE)
+a7drninae <- FindNeighbors(a7drninae, dims = 1:50, verbose = TRUE)
+a7drninae <- FindClusters(a7drninae, verbose = TRUE)
+
+a7drall <- RunPCA(a7drall, verbose = TRUE)
+a7drall <- RunUMAP(a7drall, dims = 1:50, verbose = TRUE)
+a7drall <- FindNeighbors(a7drall, dims = 1:50, verbose = TRUE)
+a7drall <- FindClusters(a7drall, verbose = TRUE)
+
 # we can plot the cluster plots at this point but the cluster labels will be seurat auto generated numbers
 DimPlot(a7dr34, label = TRUE)
 
@@ -131,6 +185,10 @@ a1dr56[["old.ident"]] <- a1d_ident
 Idents(a1dr56)<- "old.ident"
 a1dra[["old.ident"]] <- a1d_ident
 Idents(a1dra)<- "old.ident"
+a1drninae[["old.ident"]] <- a1d_ident
+Idents(a1drninae)<- "old.ident"
+a1drall[["old.ident"]] <- a1d_ident
+Idents(a1drall)<- "old.ident"
 
 a3d_ident <- Idents(a3dr)
 a3dr34[["old.ident"]] <- a3d_ident
@@ -139,6 +197,10 @@ a3dr56[["old.ident"]] <- a3d_ident
 Idents(a3dr56)<- "old.ident"
 a3dra[["old.ident"]] <- a3d_ident
 Idents(a3dra)<- "old.ident"
+a3drninae[["old.ident"]] <- a3d_ident
+Idents(a3drninae)<- "old.ident"
+a3drall[["old.ident"]] <- a3d_ident
+Idents(a3drall)<- "old.ident"
 
 a7d_ident <- Idents(a7dr)
 a7dr34[["old.ident"]] <- a7d_ident
@@ -147,6 +209,10 @@ a7dr56[["old.ident"]] <- a7d_ident
 Idents(a7dr56)<- "old.ident"
 a7dra[["old.ident"]] <- a7d_ident
 Idents(a7dra)<- "old.ident"
+a7drninae[["old.ident"]] <- a7d_ident
+Idents(a7drninae)<- "old.ident"
+a7drall[["old.ident"]] <- a7d_ident
+Idents(a7drall)<- "old.ident"
 
 # We can generate clusterplots and featureplots at this point
 # Remember that Rh3/Rh4/Rh5/Rh6 were removed from the objects and we could not generate FeaturePlots for the removed genes
@@ -155,11 +221,17 @@ Idents(a7dra)<- "old.ident"
 saveRDS(a1dr34, file = "a1d_Rh34_remove.rds")
 saveRDS(a1dr56, file = "a1d_Rh56_remove.rds")
 saveRDS(a1dra, file = "a1d_RhAll_remove.rds")
+saveRDS(a1drninae, file = "a1d_ninaE_remove.rds")
+saveRDS(a1drall, file = "a1d_ninaE_RhAll_remove.rds")
 
 saveRDS(a3dr34, file = "a3d_Rh34_remove.rds")
 saveRDS(a3dr56, file = "a3d_Rh56_remove.rds")
 saveRDS(a3dra, file = "a3d_RhAll_remove.rds")
+saveRDS(a3drninae, file = "a3d_ninaE_remove.rds")
+saveRDS(a3drall, file = "a3d_ninaE_RhAll_remove.rds")
 
 saveRDS(a7dr34, file = "a7d_Rh34_remove.rds")
 saveRDS(a7dr56, file = "a7d_Rh56_remove.rds")
 saveRDS(a7dra, file = "a7d_RhAll_remove.rds")
+saveRDS(a7drninae, file = "a7d_ninaE_remove.rds")
+saveRDS(a7drall, file = "a7d_ninaE_RhAll_remove.rds")
--- Code_for_supplemental.R
+++ Code_for_supplemental.R
@@ -18,8 +18,13 @@ cell_type_color <- c("R1-6" = "#f263df",
                      "Cone" = "#17b93f",
                      "1_pigments" = "#b69e1c",
                      "2,3_pigments" = "#f6756f")
-plot_cells(a17m, color_cells_by="assigned_cell_type", label_cell_groups = F, show_trajectory_graph = F, cell_size = 1)+scale_color_manual(values = cell_type_color) + NoAxes() + NoLegend() #S1A
-plot_cells(a17m, color_cells_by = "orig.ident", label_cell_groups = F, show_trajectory_graph = F, cell_size = 1)+NoAxes()+NoLegend() #S1B
+plot_cells(a17m, color_cells_by="assigned_cell_type", label_cell_groups = F, show_trajectory_graph = F, cell_size = 1)+scale_color_manual(values = cell_type_color) + NoAxes() + NoLegend() #S1 A
+plot_cells(a17m, color_cells_by = "orig.ident", label_cell_groups = F, show_trajectory_graph = F, cell_size = 1)+NoAxes()+NoLegend() #S1 B
+
+# load 1 day, 3 days, 7 days old male adult eyes Seurat analyzed data sets as a1d, a3d and a7d, respectively
+a17d <- merge(a1d, y = c(a3d,a7d), add.cells.ids = c("1","2","3"),project = "a1-7dmerge")
+VlnPlot(a17d, features = "nCount_RNA", group.by = "orig.ident", assay = "RNA") & NoLegend() & theme(axis.title.x = element_blank(), axis.text.x = element_text(size=20), axis.text.y = element_text(size=20)) #S1 C
+VlnPlot(a17d, features = "nFeature_RNA", group.by = "orig.ident", assay = "RNA") & NoLegend() & theme(axis.title.x = element_blank(), axis.text.x = element_text(size=20), axis.text.y = element_text(size=20)) #S1 D
 
 
 # load 1 day, 3 days, 7 days old male adult eyes Seurat analyzed data sets as a1d, a3d and a7d, respectively
@@ -100,13 +105,81 @@ FeaturePlot(a1d, features = "wrapper", pt.size = 2, order = T) + NoAxes() + NoLe
 FeaturePlot(a3d, features = "wrapper", pt.size = 2, order = T) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S7 G
 
 # Supp Fig 8
+FeaturePlot(a1d, features = "se", pt.size = 2) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 A
+FeaturePlot(a3d, features = "se", pt.size = 2) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 A
+FeaturePlot(a7d, features = "se", pt.size = 2) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 A
+FeaturePlot(a1d, features = "Pu", pt.size = 2) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 B
+FeaturePlot(a3d, features = "Pu", pt.size = 2) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 B
+FeaturePlot(a7d, features = "Pu", pt.size = 2) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 B
+
+# Supp Fig 9
+# load in 1 day old adult female eye data set as a1f
+FeaturePlot(a1f, features = "Rh3", pt.size = 2) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S9 A
+FeaturePlot(a1f, features = "Rh4", pt.size = 2) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S9 B
+FeaturePlot(a1f, features = "Skeletor", pt.size = 2, order=T) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S9 C
+FeaturePlot(a1f, features = "hth", pt.size = 2, order=T) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S9 D
+
+# Supp Fig 10
+# load in the Harmony integrated 1-day old adult male and female eye data set as a1dh
+FeaturePlot(a1dh, features = "apolpp", pt.size = 2, order=T, split.by = "orig.ident") + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S10 A and B
+
+a1dh_subset_vlnplot <- subset(a1dh, idents = c("1_Pigment","Cone"), invert = TRUE) # remove primary pigment and cone cells for the ViolinPlot
+VlnPlot(a1dh_subset_vlnplot, features = "apolpp", split.by = "orig.ident", split.plot = T) & theme(plot.title = element_text(face = "italic")) & NoLegend() #S10 C
+
+
+
+# Supp Fig 11
 # load in the following seurat objects
-# a1dr56 = 1 day old data with Rh5 and Rh6 removed
-# a3dr56 = 1 day old data with Rh5 and Rh6 removed
-# a7dr56 = 1 day old data with Rh5 and Rh6 removed
+# a1dr56 = 1-day old data with Rh5 and Rh6 removed
+# a3dr56 = 3-day old data with Rh5 and Rh6 removed
+# a7dr56 = 7-day old data with Rh5 and Rh6 removed
 FeaturePlot(a1dr56, features = "Rh3", pt.size = 2, order = T) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 A
 FeaturePlot(a3dr56, features = "Rh3", pt.size = 2, order = T) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 A
 FeaturePlot(a7dr56, features = "Rh3", pt.size = 2, order = T) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 A
 FeaturePlot(a1dr56, features = "Rh4", pt.size = 2, order = T) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 B
 FeaturePlot(a3dr56, features = "Rh4", pt.size = 2, order = T) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 B
 FeaturePlot(a7dr56, features = "Rh4", pt.size = 2, order = T) + NoAxes() + NoLegend() & theme(plot.title= element_blank()) #S8 B
+
+# Supp Fig 12
+# load in the following seurat objects
+# a1d = 1-day old data with no genes removed
+# a1dr34 = 1-day old data with Rh3 and Rh4 removed
+# a1dr56 = 1-day old data with Rh5 and Rh6 removed
+# a1dra = 1-day old data with Rh3 to Rh6 removed
+# a1drninae = 1-day old data with ninaE removed
+# a1drall = 1-day old data with ninaE, Rh3 to Rh6 removed
+
+# a3d = 3-day old data with no genes removed
+# a3dr34 = 3-day old data with Rh3 and Rh4 removed
+# a3dr56 = 3-day old data with Rh5 and Rh6 removed
+# a3dra = 3-day old data with Rh3 to Rh6 removed
+# a3drninae = 3-day old data with ninaE removed
+# a3drall = 3-day old data with ninaE, Rh3 to Rh6 removed
+
+# a7d = 7-day old data with no genes removed
+# a7dr34 = 7-day old data with Rh3 and Rh4 removed
+# a7dr56 = 7-day old data with Rh5 and Rh6 removed
+# a7dra = 7-day old data with Rh3 to Rh6 removed
+# a7drninae = 7-day old data with ninaE removed
+# a7drall = 7-day old data with ninaE, Rh3 to Rh6 removed
+
+DimPlot(a7d, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 A
+DimPlot(a7drninae, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 B
+DimPlot(a7dr34, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 C
+DimPlot(a7dr56, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 D
+DimPlot(a7dra, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 E
+DimPlot(a7drall, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 F
+
+DimPlot(a3d, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 G
+DimPlot(a3drninae, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 H
+DimPlot(a3dr34, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 I
+DimPlot(a3dr56, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 J
+DimPlot(a3dra, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 K
+DimPlot(a3drall, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 L
+
+DimPlot(a3d, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 M
+DimPlot(a3drninae, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 N
+DimPlot(a3dr34, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 O
+DimPlot(a3dr56, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 P
+DimPlot(a3dra, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 Q
+DimPlot(a3drall, pt.size = 2, label = F, cols = c("grey","grey","grey","red","blue","green3","orchid","darkorange4")) & NoAxes() & NoLegend() #S12 R
